bd35ff47c21ac05fa0a3bb6f75d8299f2198095f
Alexander Schranz <alexander@sulu.io>
2025-05-06 07:25:19 +0200

    Prepare 0.8 branch

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = 'SEAL'
 copyright = '2023, Alexander Schranz'
 author = 'Alexander Schranz'
-release = '0.7'
+release = '0.8'
 html_title = 'SEAL Documentation'
 html_short_title = 'SEAL'
 html_favicon = '_static/icons/favicon.ico'
@@ -19,7 +19,7 @@ html_context = {
     "display_github": True, # Integrate GitHub
     "github_user": "schranz-search", # Username
     "github_repo": "schranz-search", # Repo name
-    "github_version": "0.7", # Version
+    "github_version": "0.8", # Version
     "conf_py_path": "/docs/", # Path in the checkout to the docs root
 }
 
