e9a15c07ddeda78cdb3ac83e2e961f635a59db51
Mario Fehr <14167832+mario-fehr@users.noreply.github.com>
2026-02-13 08:20:50 +0100

    Add support for Elasticsearch via SSL Proxy (#649)

--- packages/seal-elasticsearch-adapter/src/ElasticsearchAdapterFactory.php
+++ packages/seal-elasticsearch-adapter/src/ElasticsearchAdapterFactory.php
@@ -45,6 +45,7 @@ class ElasticsearchAdapterFactory implements AdapterFactoryInterface
      *     port?: int,
      *     user?: string,
      *     pass?: string,
+     *     query: array<string, string|string[]>,
      * } $dsn
      */
     public function createClient(array $dsn): Client
@@ -59,8 +60,14 @@ class ElasticsearchAdapterFactory implements AdapterFactoryInterface
             return $client;
         }
 
+        $tlsQuery = $dsn['query']['tls'] ?? 'false';
+        \assert(\is_string($tlsQuery), 'The "tls" query param must be a string.');
+        $useTls = \filter_var($tlsQuery, \FILTER_VALIDATE_BOOL, \FILTER_REQUIRE_SCALAR);
+        $scheme = $useTls ? 'https' : 'http';
+        $port = $dsn['port'] ?? 9200;
+
         $client = ClientBuilder::create()->setHosts([
-            $dsn['host'] . ':' . ($dsn['port'] ?? 9200),
+            $scheme . '://' . $dsn['host'] . ':' . $port,
         ]);
 
         $user = $dsn['user'] ?? '';
