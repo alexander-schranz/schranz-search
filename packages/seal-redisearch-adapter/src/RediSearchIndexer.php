9d6b819c115638b6094ce9dbf4c6b7afd348a1e2
Alexander Schranz <alexander@sulu.io>
2026-05-27 07:46:44 +0200

    Keep Redis Marshaller addRawFilterTextField for BC Reasons (#691)
    In https://github.com/PHP-CMSIG/search/pull/689 we changed the
    Marshaller logic, why we not longer use the raw field older created
    indexes may still access them. While a drop reindex is recommended and
    in some cases required for better bc layer we still add the raw field
    for keep things working as they were before.

--- packages/seal-redisearch-adapter/src/RediSearchIndexer.php
+++ packages/seal-redisearch-adapter/src/RediSearchIndexer.php
@@ -29,6 +29,7 @@ final class RediSearchIndexer implements IndexerInterface
     ) {
         $this->marshaller = new Marshaller(
             dateFormat: 'U',
+            addRawFilterTextField: true, // remove in next bc release
             geoPointFieldConfig: [
                 'latitude' => 1,
                 'longitude' => 0,
