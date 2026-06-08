f3b3327b44386d9359c5075507d411db50b0123c
Alexander Schranz <alexander@sulu.io>
2026-06-08 09:19:46 +0200

    Fix multi word search escaping in RediSearchSearcher (#694)
    Redis when querying for a search term that contains a space, the final
    redis command is invalid and fails. So example `Other Thing` before
    ended in `%%Other%% Thing%%` instead of `%%Other%% %%Thing%%`.
    
    fixes #693
    
    Co-authored-by: jbfewo <jbfewo@users.noreply.github.com>

--- packages/seal-redisearch-adapter/src/RediSearchSearcher.php
+++ packages/seal-redisearch-adapter/src/RediSearchSearcher.php
@@ -268,7 +268,10 @@ final class RediSearchSearcher implements SearcherInterface
             };
 
             match (true) {
-                $filter instanceof Condition\SearchCondition => $filters[] = '%%' . \implode('%% ', \explode(' ', $this->escapeFilterValue($filter->query))) . '%%', // levenshtein of 2 per word
+                $filter instanceof Condition\SearchCondition => $filters[] = '%%' . \implode('%% %%', \array_map(
+                    $this->escapeFilterValue(...),
+                    \explode(' ', $filter->query),
+                )) . '%%', // levenshtein of 2 per word
                 $filter instanceof Condition\IdentifierCondition => $filters[] = '@' . $index->getIdentifierField()->name . ':{' . $this->escapeFilterValue($filter->identifier) . '}',
                 $filter instanceof Condition\EqualCondition => $filters[] = '@' . $this->getFilterField($index, $filter->field) . ':{' . $this->escapeFilterValue($this->convertValue($index, $filter->field, $filter->value)) . '}',
                 $filter instanceof Condition\NotEqualCondition => $filters[] = '-@' . $this->getFilterField($index, $filter->field) . ':{' . $this->escapeFilterValue($this->convertValue($index, $filter->field, $filter->value)) . '}',
