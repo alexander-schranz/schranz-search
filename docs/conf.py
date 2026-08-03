e9b3c973c61798ebcf75fa080082d5b5c92a10d0
AlwaysVictorious <213483686+AlwaysVictorious@users.noreply.github.com>
2026-08-03 20:53:51 +0200

    Update release version to 0.13 in conf.py (#718)

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = 'SEAL'
 copyright = '2023, Alexander Schranz'
 author = 'Alexander Schranz'
-release = '0.11'
+release = '0.13'
 html_title = 'SEAL Documentation'
 html_short_title = 'SEAL'
 html_favicon = '_static/icons/favicon.ico'
@@ -19,7 +19,7 @@ html_context = {
     "display_github": True, # Integrate GitHub
     "github_user": "PHP-CMSIG", # Username
     "github_repo": "search", # Repo name
-    "github_version": "0.11", # Version
+    "github_version": "0.13", # Version
     "conf_py_path": "/docs/", # Path in the checkout to the docs root
 }
 
